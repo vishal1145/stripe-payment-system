c018b1e425fcbab7208d81803b7a790d781712e1
vishal1145 <vishal.kumar1145@gmail.com>
2025-05-10 13:14:32 +0530

    change

--- stripe/routes/checkout.js
+++ stripe/routes/checkout.js
@@ -50,6 +50,8 @@ router.post('/create-checkout-session', async (req, res) => {
             payment_method_types: ['card'],
             line_items: lineItems,
             mode: 'subscription',
+            success_url: `${domainURL}/success.html`,
+            cancel_url: `${domainURL}/cancel.html`,
             metadata: {
                 productId,
                 productName,
